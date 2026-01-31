db7a4a2b79c6815e746fcc24b625949a996fdab2
Avelino Wambembe <aversondaniel01@gmail.com>
2026-01-31 03:13:28 +0100

    new

--- EcoTroca/src/Components/NavBar.jsx
+++ EcoTroca/src/Components/NavBar.jsx
@@ -5,7 +5,7 @@ export const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="w-full bg-gray-200 shadow-md">
+    <nav className="w-full bg-[#fafaf9] shadow-md">
       <div className="max-w-7xl mx-auto flex items-center justify-between px-8 py-4">
         {/* LOGO */}
         <div className="flex items-center gap-2">
