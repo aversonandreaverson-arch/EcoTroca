9d4d0b0e5b607cd00a885bc54488af603c6e1598
Avelino Wambembe <aversondaniel01@gmail.com>
2026-02-05 01:57:05 +0100

    mudando de logica

--- EcoTroca/src/App.jsx
+++ EcoTroca/src/App.jsx
@@ -1,15 +1,13 @@
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/home'
-import Cadastro from './Components/Cadastro'
 import Login from './pages/Login'
-import ComoFunciona from './Components/ComoFunciona'
 
 
 function App () {
   return (
     <Routes>
       <Route path='/' element={<Home />}></Route>
-      <Route path='/' element={<Login />}></Route>
+      <Route path='/Login' element={<Login />}></Route>
       
     </Routes>
   )
