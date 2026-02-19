0d8ff8abcd489864e1e3c9541bf4e2a79cbb1a43
Avelino Wambembe <aversonandreaverson@gmail.com>
2026-02-19 01:03:43 +0100

    Update CartaoColetador.jsx

--- EcoTroca/src/Components/UserProfile/CartaoColetador.jsx
+++ EcoTroca/src/Components/UserProfile/CartaoColetador.jsx
@@ -1,9 +1,28 @@
+// Este componente mostra um cartão com informações de um coletador
+// Ele recebe um objeto chamado "catador" através das props
+
 export default function CartaoColetador({ catador }) {
+
   return (
+    // Div principal do cartão
     <div className="bg-white p-4 rounded-xl shadow-sm">
-      <h3 className="font-semibold">{catador.nome}</h3>
-      <p>Total coletado: {catador.totalColetado} Kg</p>
-      <p>Município: {catador.Município}</p>
+
+      {/* Nome do coletador */}
+      <h3 className="font-semibold">
+        {catador.nome}
+      </h3>
+
+      {/* Quantidade total coletada pelo coletador */}
+      <p>
+        Total coletado: {catador.totalColetado} Kg
+      </p>
+
+      {/* Município onde o coletador atua */}
+      {/* Estamos a buscar esta informação dentro do objeto catador */}
+      <p>
+        Município: {catador.Município}
+      </p>
+
     </div>
   );
 }
