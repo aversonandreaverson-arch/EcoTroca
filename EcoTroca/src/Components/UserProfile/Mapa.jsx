06f8a38121e3c83abdcf5c6e2382842a080228c4
Avelino Wambembe <aversonandreaverson@gmail.com>
2026-02-21 23:42:19 +0100

    Update Mapa.jsx

--- EcoTroca/src/Components/UserProfile/Mapa.jsx
+++ EcoTroca/src/Components/UserProfile/Mapa.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-// eslint-disable-next-line no-undef
+ 
 export default function  Mapa  ()  {
   return (
     <div className="bg-white shadow rounded-lg p-6 mb-6">
       <h3 className="text-xl font-bold text-green-800 mb-4">Mapa 🗺️</h3>
-      <div className="h-64 bg-gray-200 flex items-center justify-center rounded-lg">
+      <div className="h-64 bg-green-200 flex items-center justify-center rounded-lg">
         {/* FUTURO: Google Maps ou Mapbox */}
         <p className="text-gray-600">Mapa em breve...</p>
       </div>
