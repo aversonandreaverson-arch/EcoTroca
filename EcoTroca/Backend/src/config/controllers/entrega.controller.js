b601a2151c3e9d69a38055241a1626eb9127f9e2
Avelino Wambembe <aversonandreaverson@gmail.com>
2026-02-23 10:43:15 +0100

    Update entrega.controller.js

--- EcoTroca/Backend/src/config/controllers/entrega.controller.js
+++ EcoTroca/Backend/src/config/controllers/entrega.controller.js
@@ -1,33 +1,33 @@
-const db = require('../config/database');
+import { execute } from '../config/database';
 
 // Criar entrega
-exports.criar = async (req, res) => {
+export async function criar(req, res) {
   const { id_ponto, data_hora } = req.body;
 
-  await db.execute(
+  await execute(
     'INSERT INTO Entrega (id_usuario, id_ponto, data_hora) VALUES (?, ?, ?)',
     [req.usuario.id, id_ponto, data_hora]
   );
 
   res.json({ mensagem: 'Entrega criada com sucesso' });
-};
+}
 
 // Aceitar entrega
-exports.aceitar = async (req, res) => {
-  await db.execute(
+export async function aceitar(req, res) {
+  await execute(
     'UPDATE Entrega SET id_coletador=?, status="aceita" WHERE id_entrega=?',
     [req.usuario.id, req.params.id]
   );
 
   res.json({ mensagem: 'Entrega aceita' });
-};
+}
 
 // Confirmar coleta
-exports.confirmar = async (req, res) => {
-  await db.execute(
+export async function confirmar(req, res) {
+  await execute(
     'UPDATE Entrega SET status="coletada" WHERE id_entrega=?',
     [req.params.id]
   );
 
   res.json({ mensagem: 'Entrega coletada com sucesso' });
-};
+}
