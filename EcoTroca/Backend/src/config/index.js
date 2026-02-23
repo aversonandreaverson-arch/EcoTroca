97ce19d2de2be19e4b3315ee8d167d51a99b8ea1
Avelino Wambembe <aversonandreaverson@gmail.com>
2026-02-23 17:19:18 +0100

    new

--- EcoTroca/Backend/src/config/index.js
+++ EcoTroca/Backend/src/config/index.js
@@ -6,7 +6,7 @@ import app from './src/app.js';
 
 // Porta do servidor
 const PORT = process.env.PORT || 3000;
-
+console.log(process.env.DB_NAME);
 // Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Servidor ECOTROCA rodando na porta ${PORT}`);
