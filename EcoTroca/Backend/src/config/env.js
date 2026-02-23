c156944084637455156da862c96c347cf4b59fa6
Avelino Wambembe <aversonandreaverson@gmail.com>
2026-02-23 17:42:56 +0100

    Update env.js

--- EcoTroca/Backend/src/config/env.js
+++ EcoTroca/Backend/src/config/env.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
 
-module.exports = {
-  PORT: process.env.PORT || 3000,
-  DB_HOST: process.env.DB_HOST,
-  DB_USER: process.env.DB_USER,
-  DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_NAME: process.env.DB_NAME,
-  JWT_SECRET: process.env.JWT_SECRET,
-};
+export const PORT = process.env.PORT || 3000;
+export const DB_HOST = process.env.DB_HOST;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_NAME = process.env.DB_NAME;
+export const JWT_SECRET = process.env.JWT_SECRET;
