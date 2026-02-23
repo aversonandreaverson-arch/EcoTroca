d0213d5f3695d64fdb3c47f74be8de33034023f4
Avelino Wambembe <aversonandreaverson@gmail.com>
2026-02-23 10:41:20 +0100

    Update status.middleware.js

---
+++ EcoTroca/Backend/src/config/middlewares/status.middleware.js
@@ -0,0 +1,15 @@
+// Middleware que bloqueia usuários desativados
+import { execute } from '../config/database';
+
+export default async (req, res, next) => {
+  const [[usuario]] = await execute(
+    'SELECT ativo FROM Usuario WHERE id_usuario = ?',
+    [req.usuario.id]
+  );
+
+  if (!usuario || !usuario.ativo) {
+    return res.status(403).json({ erro: 'Conta desativada' });
+  }
+
+  next();
+};
