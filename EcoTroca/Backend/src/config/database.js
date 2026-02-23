27d8a4f743856e53bbfc5bbd52a21679da5a9845
Avelino Wambembe <aversonandreaverson@gmail.com>
2026-02-23 14:35:08 +0100

    Update database.js

--- EcoTroca/Backend/src/config/database.js
+++ EcoTroca/Backend/src/config/database.js
@@ -6,7 +6,7 @@ const pool = createPool({
   host: process.env.DB_HOST,      // endereço do banco
   user: process.env.DB_USER,      // usuário
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME 
 });
 
 // Exporta usando Promise para async/await
