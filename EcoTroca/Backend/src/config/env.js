71845344fb429fd3271c3ba240f709c65f4c5648
Avelino Wambembe <aversonandreaverson@gmail.com>
2026-02-23 17:39:02 +0100

    Update env.js

--- EcoTroca/Backend/src/config/env.js
+++ EcoTroca/Backend/src/config/env.js
@@ -1,8 +1,10 @@
-// Importa o pacote dotenv
-import dotenv from 'dotenv';
+require('dotenv').config();
 
-// Carrega as variáveis do arquivo .env para process.env
-dotenv.config();
-
-// Exporta apenas para garantir que este arquivo seja executado
-export default {};
+module.exports = {
+  PORT: process.env.PORT || 3000,
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
+  JWT_SECRET: process.env.JWT_SECRET,
+};
