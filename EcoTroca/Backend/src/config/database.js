6376fd40532a350858f683712955ce6e7dc7eb02
Avelino Wambembe <aversonandreaverson@gmail.com>
2026-02-23 19:35:41 +0100

    Update database.js

--- EcoTroca/Backend/src/config/database.js
+++ EcoTroca/Backend/src/config/database.js
@@ -1,5 +1,5 @@
 import { createPool } from 'mysql2/promise';
-import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } from './env';
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } from './env.js';
 
 const pool = createPool({
   host: DB_HOST,
