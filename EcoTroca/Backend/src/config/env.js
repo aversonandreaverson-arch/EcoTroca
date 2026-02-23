a6839b3d550d21f94b054cb8c4de50802dfc0e56
Avelino Wambembe <aversonandreaverson@gmail.com>
2026-02-23 14:37:09 +0100

    Create env.js

---
+++ EcoTroca/Backend/src/config/env.js
@@ -0,0 +1,8 @@
+// Importa o pacote dotenv
+import dotenv from 'dotenv';
+
+// Carrega as variáveis do arquivo .env para process.env
+dotenv.config();
+
+// Exporta apenas para garantir que este arquivo seja executado
+export default {};
