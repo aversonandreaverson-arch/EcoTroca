565a8c4eb13024d4d5cb0507765f561bd1560e7c
Avelino Wambembe <aversonandreaverson@gmail.com>
2026-02-23 10:47:20 +0100

    Update admin.controller.js

--- EcoTroca/Backend/src/config/controllers/admin.controller.js
+++ EcoTroca/Backend/src/config/controllers/admin.controller.js
@@ -1,11 +1,11 @@
-const db = require('../config/database');
+import { execute } from '../config/database';
 
 // Banir usuário
-exports.banir = async (req, res) => {
-  await db.execute(
+export async function banir(req, res) {
+  await execute(
     'UPDATE Usuario SET ativo=FALSE WHERE id_usuario=?',
     [req.params.id]
   );
 
   res.json({ mensagem: 'Usuário desativado' });
-};
+}
