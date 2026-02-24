c3a7645a23d9f37feaf0fc61d66da53f8fa22484
Avelino Wambembe <aversonandreaverson@gmail.com>
2026-02-24 17:38:04 +0100

    Update App.js

--- EcoTroca/Backend/src/App.js
+++ EcoTroca/Backend/src/App.js
@@ -4,6 +4,9 @@ import authRoutes from './routes/auth.routes.js';
 import usuarioRoutes from './routes/usuario.routes.js';
 import entregaRoutes from './routes/entrega.routes.js';
 import coletadorRoutes from './routes/coletador.routes.js';
+import empresaRoutes from './routes/empresa.routes.js';
+import adminRoutes from './routes/admin.routes.js';
+
 const app = express();
 
 app.use(cors());
@@ -14,6 +17,9 @@ app.use('/api/auth', authRoutes);
 app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/entregas', entregaRoutes);
 app.use('/api/coletador', coletadorRoutes);
+app.use('/api/empresas', empresaRoutes);
+app.use('/api/admin', adminRoutes);
+
 // (as outras rotas vamos adicionar aqui depois)
 
 app.get('/', (req, res) => res.json({ mensagem: 'EcoTroca API funcionando!' }));
