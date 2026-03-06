686d80840de23ef6ba5c9a155876d124e0ffc344
Avelino Wambembe <aversonandreaverson@gmail.com>
2026-03-06 12:09:55 +0100

    Update Educacao.jsx

--- EcoTroca/src/Components/UserProfile/Educacao.jsx
+++ EcoTroca/src/Components/UserProfile/Educacao.jsx
@@ -1,7 +1,4 @@
-// ============================================================
-//  Educacao.jsx — Utilizador Comum
-//  Substitui: src/Components/UserProfile/Educacao.jsx
-// ============================================================
+
 import React from 'react';
 import EducacaoBase from '../EducacaoBase';
 import Header from './Header';
